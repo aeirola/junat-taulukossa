7bb6a6a2e9aca603bcf3f9d254633521126c2c0f
Axel Eirola <axel.eirola@iki.fi>
2012-01-15 14:10:44 +0200

    Added proxies.

---
+++ juna_proxy.php
@@ -0,0 +1,10 @@
+<?php
+$url = "http://188.117.35.14/TrainRSS/TrainService.svc/trainInfo?train=" + $_GET["train"];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+curl_close($ch);
+echo $output;
+?>
